f5a4a70d5f9383c8913ace9b405e53541996c56a
dsilentkiller <paarurawal55@gmail.com>
2024-03-19 09:22:46 +0545

    added list_display field for StockReportAdmin in ventory to filter in admin panel

--- inventory/admin.py
+++ inventory/admin.py
@@ -5,5 +5,15 @@ admin.site.register(Inventory)
 admin.site.register(Order)
 admin.site.register(Category)
 admin.site.register(Table)
-admin.site.register(StockReport)
+
 admin.site.register(Purchase)
+
+
+class StockReportAdmin(admin.ModelAdmin):
+    list_display = ('inventory', 'order', 'stock_remain')
+
+    def stock_remain(self, obj):
+        return obj.stock_remain
+
+
+admin.site.register(StockReport, StockReportAdmin)
